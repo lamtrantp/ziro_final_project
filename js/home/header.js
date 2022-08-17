84b4244f17f40de01fa5d5ec22b433d4b6be71c6
tqlam <tranquanglamtp@gmail.com>
2022-08-17 12:27:40 +0700

    js header

--- js/home/header.js
+++ js/home/header.js
@@ -1,15 +1,44 @@
-// let input = document.querySelector("input.header_nav_input")
-// let buttonImage = document.querySelector("img.img-button")
+let isOpen = false;
+let button = document.getElementById("nav_menu_icon");
+button.addEventListener("click", function() {
+    if (isOpen) {
+        isOpen = false;
+        document.getElementById("main").style.top = "0";
+        // document.getElementById("header_nav_secondary").style.opacity = "0";
+    } else {
+        isOpen = true;
+        document.getElementById("main").style.top = "230px";
+        // document.getElementById("header_nav_secondary").style.opacity = "1";
+    }
+})
 
+let collapeTwoOpen = false;
+let height = document.querySelector("div.collapetwo").clientHeight;
+document.querySelectorAll("div.collapetwo").forEach ((item, index) => {
+    console.log(height);
+    item.style.height = "0px";
+})
 
+const btns = document.querySelectorAll("div.accordion_item")
+btns.forEach ((item, index) => {
 
-// buttonImage.addEventListener('click', function(event) {
-//     event.preventDefault();
+    item.addEventListener("click", event => {
+        console.log(index);
+        console.log(event.target, index);
 
-//     console.log("123123", input.value)
-// })
+        let action = event.target.querySelector("div.collapetwo").style.height;
+        
+        console.log(action);
 
-// let formInput = document.querySelector("input.form_control")
-// document.querySelector("button.form_btn_inner_sub").onclick = function () {
-//     console.log(formInput.value)
-// }
+        btns.forEach ((item, index) => {
+            console.log(item);
+            item.querySelector(".collapetwo").style.height = "0px";
+        })
+        
+        if (action === "0px") {
+            event.target.querySelector(".collapetwo").style.height = height + "px";
+        } else {
+            event.target.querySelector(".collapetwo").style.height = "0px";
+        }
+    })
+})
